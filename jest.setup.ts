9a1a87fd1e68ce5a145d6760070c4350c7e241f6
catheisabelle <cathy070606@gmail.com>
2026-04-08 16:39:40 +0700

    commit 7
    Added polyfills for fetch and related globals to support Firebase auth in Node.js test environment.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom"
+
+
+// Polyfill fetch and related globals for Firebase auth in Node.js test environment
+const fetch = require('node-fetch')
+global.fetch = fetch
+global.Response = fetch.Response
+global.Request = fetch.Request
+global.Headers = fetch.Headers
